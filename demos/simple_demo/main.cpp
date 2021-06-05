be2d9ae9eac67ea9720fac7b2ddbc362ae803655
wanxwang <wanxin-wang@uiowa.edu>
2021-06-05 14:56:44 -0500

    reset initial posture

--- demos/simple_demo/main.cpp
+++ demos/simple_demo/main.cpp
@@ -3,21 +3,43 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
-    {
-        std::cerr << "Usage: simpe_demo [BVH_PATH]" << std::endl;
-        std::cout << "Loading a default BVH file..." << std::endl;
-    }
-    
-    const std::string bvh_file_path = (argc >= 2) ? argv[1] : "131_03.bvh";
+	bool for_show_file_info = (2 == argc);
+	bool for_reset_restpose = (4 == argc);
+	if (!for_show_file_info
+	 && !for_reset_restpose)
+	{
+		std::cerr << "Usage:\tsimpe_demo [BVH_PATH]\t\t//to show the file information" << std::endl;
+		std::cerr <<       "\tsimpe_demo [BVH_PATH_SRC] [FRAME_NO] [BVH_PATH_DEST]\t//to reset rest posture with the given frame posture" << std::endl;
 
-    bvh11::BvhObject bvh(bvh_file_path);
-    
-    std::cout << "#Channels       : " << bvh.channels().size() << std::endl;
-    std::cout << "#Frames         : " << bvh.frames()          << std::endl;
-    std::cout << "Frame time      : " << bvh.frame_time()      << std::endl;
-    std::cout << "Joint hierarchy : " << std::endl;
-    bvh.PrintJointHierarchy();
-    
-    return 0;
+	}
+	else
+	{
+		const std::string bvh_file_path = argv[1];
+
+		bvh11::BvhObject bvh(bvh_file_path);
+
+		if (for_show_file_info)
+		{
+			std::cout << "#Channels       : " << bvh.channels().size() << std::endl;
+			std::cout << "#Frames         : " << bvh.frames()          << std::endl;
+			std::cout << "Frame time      : " << bvh.frame_time()      << std::endl;
+			std::cout << "Joint hierarchy : " << std::endl;
+			bvh.PrintJointHierarchy();
+		}
+		else
+		{
+			const std::string bvh_file_path_dst = argv[3];
+			int n_frame = atoi(argv[2]);
+			if (bvh.ResetRestPose(n_frame))
+				bvh.WriteBvhFile(bvh_file_path_dst);
+			else
+			{
+				std::cout << "Please input a right frame number between 1 and " << bvh.frames() << " !!!" << std::endl;
+			}
+		}
+	}
+
+
+
+	return 0;
 }
