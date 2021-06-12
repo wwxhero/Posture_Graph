b299f360215fc17178969783787687fd0c9d5c8d
wanxwang <wanxin-wang@uiowa.edu>
2021-06-12 18:28:56 -0500

    standardized names

--- demos/simple_demo/main.cpp
+++ demos/simple_demo/main.cpp
@@ -19,7 +19,7 @@ HBODY create_arti_body(bvh11::BvhObject& bvh, Joint_bvh_ptr j_bvh, int frame)
 		{rq.w(), rq.x(), rq.y(), rq.z()},
 		{tt.x(), tt.y(), tt.z()}
 	};
-	auto b_hik = create_arti_body_c(name, &tm_hik);
+	auto b_hik = create_tree_body_node_c(name, &tm_hik);
 	return b_hik;
 }
 
@@ -46,7 +46,7 @@ HBODY create_arti_body_as_rest_bvh_pose(bvh11::BvhObject& bvh, Joint_bvh_ptr j_b
 		{1, 0, 0, 0},
 		{tt.x(), tt.y(), tt.z()}
 	};
-	auto b_hik = create_arti_body_c(name, &tm_hik);
+	auto b_hik = create_tree_body_node_c(name, &tm_hik);
 	return b_hik;
 }
 
@@ -220,7 +220,7 @@ inline void TraverseDFS_botree_nonrecur(HBODY root, LAMaccessEnter OnEnterBody, 
 		HBODY body_child;
 	} EDGE;
 	std::stack<EDGE> stkDFS;
-	stkDFS.push({root, get_first_child(root)});
+	stkDFS.push({root, get_first_child_body(root)});
 	//printArtName(body_name_w(root), 0);
 	OnEnterBody(root);
 	while (!stkDFS.empty())
@@ -236,8 +236,8 @@ inline void TraverseDFS_botree_nonrecur(HBODY root, LAMaccessEnter OnEnterBody, 
 		{
 			//printArtName(body_name_w(edge.body_child), n_indent);
 			OnEnterBody(edge.body_child);
-			HBODY body_grandchild = get_first_child(edge.body_child);
-			HBODY body_nextchild = get_next_sibling(edge.body_child);
+			HBODY body_grandchild = get_first_child_body(edge.body_child);
+			HBODY body_nextchild = get_next_sibling_body(edge.body_child);
 			stkDFS.push({edge.body_child, body_grandchild});
 			edge.body_child = body_nextchild;
 		}
@@ -403,14 +403,27 @@ bool ResetRestPose(bvh11::BvhObject& bvh, int t)
 		updateAnim(h_drivee, bvh);
 	}
 
-	auto lam_onEnter = [] (HMOTIONNODE node)
-							{
-							};
-	auto lam_onLeave = [] (HMOTIONNODE node)
-							{
-								destroy_tree_motion_node(node);
-							};
-	TraverseDFS_motree_nonrecur(h_motion_driver, lam_onEnter, lam_onLeave);
+	{
+		auto lam_onEnter = [] (HMOTIONNODE node)
+								{
+								};
+		auto lam_onLeave = [] (HMOTIONNODE node)
+								{
+									destroy_tree_motion_node(node);
+								};
+		// TraverseDFS_motree_nonrecur(h_motion_driver, lam_onEnter, lam_onLeave);
+	}
+
+	{
+		auto lam_onEnter = [] (HBODY node)
+								{
+								};
+		auto lam_onLeave = [] (HBODY node)
+								{
+									destroy_tree_body_node(node);
+								};
+		TraverseDFS_botree_nonrecur(h_driver, lam_onEnter, lam_onLeave);
+	}
 	return true;
 }
 
