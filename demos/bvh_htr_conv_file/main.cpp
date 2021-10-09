1832323540e1561acf20dbfd74a554e1b3f7e516
wanxwang <wanxin-wang@uiowa.edu>
2021-10-09 15:31:15 -0500

    (record)htr->bvh(playback) is verified

--- demos/bvh_htr_conv_file/main.cpp
+++ demos/bvh_htr_conv_file/main.cpp
@@ -16,10 +16,6 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		auto convert = [](const char* src, const char* dst, bool htr2bvh)
-		{
-			std::cout << src << "\t" << dst << "\t" << "htr2bvh=" << htr2bvh << std::endl;
-		};
 		const char* c_exts[] = { ".htr", ".bvh" };
 		fs::path path_src(argv[1]);
 		fs::path path_dst(argv[2]);
@@ -27,7 +23,15 @@ int main(int argc, char* argv[])
 		bool htr2bvh = (exts_input[0] == c_exts[0] && exts_input[1] == c_exts[1]);
 		bool bvh2htr = (exts_input[0] == c_exts[1] && exts_input[1] == c_exts[0]);
 		if (htr2bvh || bvh2htr)
-			convert(argv[1], argv[2], htr2bvh);
+		{
+			auto tick_start = ::GetTickCount64();
+			bool converted = convert(argv[1], argv[2], htr2bvh);
+			auto tick = ::GetTickCount64() - tick_start;
+			float tick_sec = tick / 1000.0f;
+			const char* res[] = { "failed", "successful" };
+			int i_res = (converted ? 1 : 0);
+			printf("Converting %s to %s takes %.2f seconds: %s\n", argv[1], argv[2], tick_sec, res[i_res]);
+		}
 		else
 		{
 			std::cout << "Not the right file extensions!!!" << std::endl;
