bdd214ec12b126401d08c92ab5d2761309d2b94f
wanxwang <wanxin-wang@uiowa.edu>
2021-10-21 09:48:55 -0500

    posture graph generation test set

--- demos/posture_graph_gen_file/main.cpp
+++ demos/posture_graph_gen_file/main.cpp
@@ -4,44 +4,40 @@
 #include <string>
 #include <shlwapi.h>
 #include <strsafe.h>
-#include "posture_graph.h"
 #include "filesystem_helper.hpp"
+#include "posture_graph.h"
+
 
 int main(int argc, char* argv[])
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 
-	if (4 != argc)
+	if (3 != argc)
 	{
-		std::cout << "Usage:\tposture_error_vis <INTERESTS> <HTR> <PNG_grayscale>" << std::endl;
+		std::cout << "Usage:\tposture_graph_gen_file <HTR> <PG_DIR>" << std::endl;
 		return -1;
 	}
 	else
 	{
 		try
 		{
-			const char* c_exts[] = { ".xml", ".htr", ".png" };
-			const char* path_interests_conf = argv[1];
-			const char* path_src = argv[2];
-			const char* path_dst = argv[3];
+			const char* c_exts[] = {".htr"};
+			const char* path_htr = argv[1];
+			const char* pg_dir = argv[2];
 			std::string exts_input[] = {
-				Norm(fs::path(path_interests_conf).extension().u8string()),
-				Norm(fs::path(path_src).extension().u8string()),
-				Norm(fs::path(path_dst).extension().u8string())
+				Norm(fs::path(path_htr).extension().u8string()),
 			};
-			bool htr2png = (exts_input[0] == c_exts[0] 
-							&& exts_input[1] == c_exts[1]
-							&& exts_input[2] == c_exts[2]);
-			if (htr2png)
+			bool htr2pg = (exts_input[0] == c_exts[0]);
+			if (htr2pg)
 			{
 				const char* res[] = { "failed", "successful" };
 				auto tick_start = ::GetTickCount64();
-				bool done = err_vis(path_interests_conf, path_src, path_dst);
+				bool done = posture_graph_gen(path_htr, pg_dir);
 				int i_res = done ? 1 : 0;
 				auto tick = ::GetTickCount64() - tick_start;
 				float tick_sec = tick / 1000.0f;
-				printf("Converting error-table from %s to %s takes %.2f seconds: %s\n", argv[1], argv[2], tick_sec, res[i_res]);
+				printf("Converting error-table from %s to %s takes %.2f seconds: %s\n", path_htr, pg_dir, tick_sec, res[i_res]);
 			}
 			else
 			{
