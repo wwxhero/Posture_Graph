42f60f4c49aed86aa426d83717317167193d913f
wanxwang <wanxin-wang@uiowa.edu>
2021-10-21 09:15:22 -0500

    error vis with interests specification

--- demos/posture_error_vis/main.cpp
+++ demos/posture_error_vis/main.cpp
@@ -12,30 +12,36 @@ int main(int argc, char* argv[])
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 
-	if (3 != argc)
+	if (4 != argc)
 	{
-		std::cout << "Usage:\tposture_error_vis <HTR> <PNG_grayscale>" << std::endl;
+		std::cout << "Usage:\tposture_error_vis <INTERESTS> <HTR> <PNG_grayscale>" << std::endl;
 		return -1;
 	}
 	else
 	{
 		try
 		{
-			// std::cerr << "Usage:\tposture_error_vis <HTR> <PNG_grayscale>" << std::endl;
-			const char* c_exts[] = { ".htr", ".png" };
-			fs::path path_src(argv[1]);
-			fs::path path_dst(argv[2]);
-			std::string exts_input[] = { Norm(path_src.extension().u8string()), Norm(path_dst.extension().u8string()) };
-			bool htr2png = (exts_input[0] == c_exts[0] && exts_input[1] == c_exts[1]);
+			const char* c_exts[] = { ".xml", ".htr", ".png" };
+			const char* path_interests_conf = argv[1];
+			const char* path_src = argv[2];
+			const char* path_dst = argv[3];
+			std::string exts_input[] = {
+				Norm(fs::path(path_interests_conf).extension().u8string()),
+				Norm(fs::path(path_src).extension().u8string()),
+				Norm(fs::path(path_dst).extension().u8string())
+			};
+			bool htr2png = (exts_input[0] == c_exts[0] 
+							&& exts_input[1] == c_exts[1]
+							&& exts_input[2] == c_exts[2]);
 			if (htr2png)
 			{
-				const char* path_htr = argv[1];
-				const char* path_png = argv[2];
+				const char* res[] = { "failed", "successful" };
 				auto tick_start = ::GetTickCount64();
-				err_vis(path_htr, path_png);
+				bool done = err_vis(path_interests_conf, path_src, path_dst);
+				int i_res = done ? 1 : 0;
 				auto tick = ::GetTickCount64() - tick_start;
 				float tick_sec = tick / 1000.0f;
-				printf("Converting error-table from %s to %s takes %.2f seconds\n", argv[1], argv[2], tick_sec);
+				printf("Converting error-table from %s to %s takes %.2f seconds: %s\n", argv[1], argv[2], tick_sec, res[i_res]);
 			}
 			else
 			{
