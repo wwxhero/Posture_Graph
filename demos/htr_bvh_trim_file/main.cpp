20fba1712a365f00ce7d822b69c8843f31955807
wanxwang <wanxin-wang@uiowa.edu>
2021-11-12 16:35:40 -0600

    CC trim file

--- demos/htr_bvh_trim_file/main.cpp
+++ demos/htr_bvh_trim_file/main.cpp
@@ -14,35 +14,29 @@ int main(int argc, char* argv[])
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 
-	if (argc < 5)
+	if (argc < 4)
 	{
-		std::cout << "Usage:\thtr_bvh_trim <DIR_SRC> <DIR_DST> <FILE_NAME> <SubTreeRM>+ \t\t\t\t\t// to trim the articulated body tree file <FILE_NAME> stored in <DIR_SRC>, the trimming result is stored in <DIR_DST>" << std::endl;
+		std::cout << "Usage:\thtr_bvh_trim <FILE_SRC> <FILE_DST> <SubTreeRM>+ \t\t\t\t\t// to trim the articulated body tree file <FILE_SRC>, the trimming result is stored in <FILE_DST>" << std::endl;
 	}
 	else
 	{
-		const int i_base_subtree = 4;
+		const int i_base_subtree = 3;
 		int n_subtrees_rm = argc-i_base_subtree;
 		std::vector<const char*> name_subtrees_rm(n_subtrees_rm);
 		for (int i_subtree = 0; i_subtree < n_subtrees_rm; i_subtree ++)
 			name_subtrees_rm[i_subtree] = argv[i_subtree + i_base_subtree];
 
-		auto OnTrimFile = [&name_subtrees_rm = std::as_const(name_subtrees_rm)](const char* path_src, const char* path_dst) -> bool
-			{
-				std::cout << "Trimming " << path_src << " to " << path_dst << ":";
-				bool trimmed = trim(path_src, path_dst, name_subtrees_rm.data(), (int)name_subtrees_rm.size());
-				static const char* const results[] = {"failed", "successful"};
-				int i_result = trim? 1 : 0;
-				std::cout << results[i_result] << std::endl;
-				return true;
-			};
-
-		const char* dir_src = argv[1];
-		const char* dir_dst = argv[2];
-		const char* file_name = argv[3];
+		const char* path_src = argv[1];
+		const char* path_dst = argv[2];
 
 		try
 		{
-			CopyDirTree_file(dir_src, dir_dst, OnTrimFile, file_name);
+			std::cout << "Trimming " << path_src << " to " << path_dst << ":";
+			bool trimmed = trim(path_src, path_dst, name_subtrees_rm.data(), (int)name_subtrees_rm.size());
+			if (trimmed)
+				std::cout << "successful!" << std::endl;
+			else
+				std::cout << "failed!!!" << std::endl;
 		}
 		catch(const std::string& exp)
 		{
