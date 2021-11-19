1a2d68d4140895b12b8494d5ef98afaec493030f
wanxwang <wanxin-wang@uiowa.edu>
2021-11-18 19:20:42 -0600

    tempory solution of merging posture graphs, will be changed soon

--- demos/posture_graph_merge_file/main.cpp
+++ demos/posture_graph_merge_file/main.cpp
@@ -13,40 +13,29 @@ int main(int argc, char* argv[])
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 
-	if (5 != argc)
+	if (6 != argc)
 	{
-		std::cout << "Usage:\tposture_graph_gen_file <INTEREST_XML> <HTR> <PG_DIR> <Epsilon>" << std::endl;
+		std::cout << "Usage:\tposture_graph_gen_file <INTEREST_XML> <PG_DIR_0> <PG_DIR_1> <PG_NAME> <Epsilon>" << std::endl;
 		return -1;
 	}
 	else
 	{
 		try
 		{
-			const char* c_exts[] = {".htr"};
 			const char* path_interests_conf = argv[1];
-			const char* path_htr = argv[2];
-			const char* pg_dir = argv[3];
-			Real eps_err = (Real)atof(argv[4]);
-			std::string exts_input[] = {
-				Norm(fs::path(path_htr).extension().u8string()),
-			};
-			bool htr2pg = (exts_input[0] == c_exts[0]);
-			if (htr2pg)
-			{
-				printf("Converting error-table from %s to %s ", path_htr, pg_dir);
-				const char* res[] = { "failed", "successful" };
-				auto tick_start = ::GetTickCount64();
-				bool done = posture_graph_gen(path_interests_conf, path_htr, pg_dir, eps_err, NULL);
-				int i_res = done ? 1 : 0;
-				auto tick = ::GetTickCount64() - tick_start;
-				float tick_sec = tick / 1000.0f;
-				printf("takes %.2f seconds: %s\n", tick_sec, res[i_res]);
-			}
-			else
-			{
-				std::cout << "Not the right file extensions!!!" << std::endl;
-			}
-			
+			const char* pg_dir_0 = argv[2];
+			const char* pg_dir_1 = argv[3];
+			const char* pg_name = argv[4];
+			Real eps_err = (Real)atof(argv[5]);
+
+			printf("Merging %s and %s for files %s.htr and %s.pg ", pg_dir_0, pg_dir_1, pg_name, pg_name);
+			const char* res[] = { "failed", "successful" };
+			auto tick_start = ::GetTickCount64();
+			bool done = posture_graph_merge(path_interests_conf, pg_dir_0, pg_dir_1, pg_name, eps_err);
+			int i_res = done ? 1 : 0;
+			auto tick = ::GetTickCount64() - tick_start;
+			float tick_sec = tick / 1000.0f;
+			printf("takes %.2f seconds: %s\n", tick_sec, res[i_res]);
 		}
 		catch (std::string &info)
 		{
