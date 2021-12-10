f083d99f05c93b11f790721b282be4d91ec17595
wanxwang <wanxin-wang@uiowa.edu>
2021-12-09 20:26:37 -0600

    build pg batch

--- build_pg.bat
+++ build_pg.bat
@@ -9,6 +9,8 @@ Set HTR_raw_dir=%PG_dir_dst%\Intermediate_HTR_raw_1
 Set HTR_disect_dir=%PG_dir_dst%\Intermediate_HTR_disect_2
 Set HTR_trimmed_dir=%PG_dir_dst%\Intermediate_HTR_trimmed_3
 Set HTR_dissect_XML=%BVH_dir_src%\disection.xml
+Set PG_Interests_XML=%BVH_dir_src%\pg_interests.xml
+Set PGS_dir_dst=%PG_dir_dst%\Intermediate_PGS_4
 
 setlocal enabledelayedexpansion
 
@@ -26,15 +28,15 @@ IF NOT %argCount% == 4 (
 
 	mkdir %T_reset_dir%
 	echo bvh_posture_reset_parallel %BVH_dir_src% %T_reset_dir% %N_threads% ">" bvh_posture_reset.log
-	rem bvh_posture_reset_parallel %BVH_dir_src% %T_reset_dir% %N_threads% > bvh_posture_reset.log
+	bvh_posture_reset_parallel %BVH_dir_src% %T_reset_dir% %N_threads% > bvh_posture_reset.log
 
 	mkdir %HTR_raw_dir%
 	echo bvh_htr_conv_parallel %T_reset_dir% %HTR_raw_dir% %N_threads% ">" bvh_tr_conv.log
-	rem bvh_htr_conv_parallel %T_reset_dir% %HTR_raw_dir% %N_threads% > bvh_tr_conv.log
+	bvh_htr_conv_parallel %T_reset_dir% %HTR_raw_dir% %N_threads% > bvh_tr_conv.log
 
 	mkdir %HTR_disect_dir%
 	echo htr_dissect_parallel %HTR_dissect_XML% %HTR_raw_dir% %HTR_disect_dir% %N_threads% ">" htr_dissect.log
-	rem htr_dissect_parallel %HTR_dissect_XML% %HTR_raw_dir% %HTR_disect_dir% %N_threads% > htr_dissect.log
+	htr_dissect_parallel %HTR_dissect_XML% %HTR_raw_dir% %HTR_disect_dir% %N_threads% > htr_dissect.log
 	copy %HTR_dissect_XML% %HTR_disect_dir%
 
 	mkdir %HTR_trimmed_dir%
@@ -53,35 +55,17 @@ IF NOT %argCount% == 4 (
 	echo htr_bvh_trim_parallel %HTR_disect_dir% %HTR_trimmed_dir% LowerBack.htr %N_threads% Head LeftShoulder RightShoulder ">>" htr_trimmed.log
 	htr_bvh_trim_parallel %HTR_disect_dir% %HTR_trimmed_dir% LowerBack.htr %N_threads% Head LeftShoulder RightShoulder >> htr_trimmed.log
 
+	mkdir %PGS_dir_dst%
+	rem to generate posture graphs for each HTR file stored in BVH_DIR_SRC
+	echo posture_graph_gen_parallel %PG_Interests_XML% %HTR_trimmed_dir% %PGS_dir_dst% %Eps% %N_threads% ">" posture_graph_gen_parallel.log
+	posture_graph_gen_parallel %PG_Interests_XML% %HTR_trimmed_dir% %PGS_dir_dst% %Eps% %N_threads% > posture_graph_gen_parallel.log
+	copy %PG_Interests_XML% %PGS_dir_dst%
+
+	rem to merge all the PGs into one PG
+	rem it is a risk to memory system running 5 parts in parallel
+	echo posture_graph_merge_parallel %PG_Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LowerBack LeftUpLeg RightUpLeg LeftArm RightArm ">" posture_graph_merge_parallel.log
+	posture_graph_merge_parallel %PG_Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LowerBack LeftUpLeg RightUpLeg LeftArm RightArm > posture_graph_merge_parallel.log
+	copy %PG_Interests_XML% %PG_dir_dst%
+
+	exit 0
 )
-
-
-
-
-rem rem usage: pg <INTERESTS_XML> <BVH_DIR_SRC> <PGS_DIR_DST> <Epsilon> <N_threads>
-rem rem posture_graph_gen_parallel <INTERESTS_XML> <BVH_DIR_SRC> <PGS_DIR_DST> <Epsilon> <N_threads>
-rem rem posture_graph_merge <INTERESTS_XML> <PG_DIR_SRC> <PG_DIR_DST> <PG_NAME> <Epsilon> <N_threads>
-rem Set Interests_XML=%1
-rem Set BVH_dir_src_trimmed=%2
-rem Set PG_dir_dst=%3
-rem Set Eps=%4
-rem Set /A Eps_merge = Eps+1
-rem Set N_threads=%5
-rem Set PGS_dir_dst=%PG_dir_dst%\Intermediate
-rem mkdir %PGS_dir_dst%
-rem rem to generate posture graphs for each HTR file stored in BVH_DIR_SRC
-rem echo posture_graph_gen_parallel %Interests_XML% %BVH_dir_src_trimmed% %PGS_dir_dst% %Eps% %N_threads% ">" posture_graph_gen_parallel.log
-rem posture_graph_gen_parallel %Interests_XML% %BVH_dir_src_trimmed% %PGS_dir_dst% %Eps% %N_threads% > posture_graph_gen_parallel.log
-
-rem rem to merge all the PGs into one PG
-
-rem rem echo posture_graph_merge_parallel %Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LowerBack LeftUpLeg RightUpLeg ">" posture_graph_merge_parallel.log
-rem rem posture_graph_merge_parallel %Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LowerBack LeftUpLeg RightUpLeg > posture_graph_merge_parallel.log
-
-rem rem echo posture_graph_merge_parallel %Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LeftArm RightArm ">>" posture_graph_merge_parallel.log
-rem rem posture_graph_merge_parallel %Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LeftArm RightArm >> posture_graph_merge_parallel.log
-
-rem rem it is a risk to memory system running 5 parts in parallel
-rem echo posture_graph_merge_parallel %Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LowerBack LeftUpLeg RightUpLeg LeftArm RightArm ">" posture_graph_merge_parallel.log
-rem posture_graph_merge_parallel %Interests_XML% %PGS_dir_dst% %PG_dir_dst% %Eps% LowerBack LeftUpLeg RightUpLeg LeftArm RightArm > posture_graph_merge_parallel.log
-
