fdf047f87f0624d3d04d0516bef3805a2e458736
wanxwang <wanxin-wang@uiowa.edu>
2022-01-03 13:30:26 -0600

    remove theta noise

--- demos/remove_theta_noise/main.cpp
+++ demos/remove_theta_noise/main.cpp
@@ -10,31 +10,19 @@ int main(int argc, char* argv[])
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
 
-	if (3 != argc)
+	if (4 != argc)
 	{
-		std::cerr << "Usage:\tdot_pg_conv_file <PATH_SRC> <PATH_DST>\t\t//to convert file <PATH_SRC> to <PATH_DST>" << std::endl;
+		std::cerr << "Usage:\tdot_pg_conv_file <PATH_SRC> <PATH_DST> <PATH_INTERESTS_CONF>\t\t//to remove theta noise of <PATH_SRC>, and save it to <PATH_DST>" << std::endl;
 	}
 	else
 	{
-		const char* c_exts[] = { ".pg", ".dot" };
-		fs::path path_src(argv[1]);
-		fs::path path_dst(argv[2]);
-		std::string exts_input[] = { path_src.extension().u8string(), path_dst.extension().u8string() };
-		bool pg2dot = (exts_input[0] == c_exts[0] && exts_input[1] == c_exts[1]);
-		if (pg2dot)
-		{
-			auto tick_start = ::GetTickCount64();
-			bool converted = convert_pg2dot(argv[1], argv[2]);
-			auto tick = ::GetTickCount64() - tick_start;
-			float tick_sec = tick / 1000.0f;
-			const char* res[] = { "failed", "successful" };
-			int i_res = (converted ? 1 : 0);
-			printf("Converting %s to %s takes %.2f seconds: %s\n", argv[1], argv[2], tick_sec, res[i_res]);
-		}
-		else
-		{
-			std::cout << "Not the right file extensions!!!" << std::endl;
-		}
+		auto tick_start = ::GetTickCount64();
+		bool removed = remove_theta_noise(argv[1], argv[2], argv[3]);
+		auto tick = ::GetTickCount64() - tick_start;
+		float tick_sec = tick / 1000.0f;
+		const char* res[] = { "failed", "successful" };
+		int i_res = (removed ? 1 : 0);
+		printf("Removing noise from %s, and saving it to %s takes %.2f seconds: %s\n", argv[1], argv[2], tick_sec, res[i_res]);
 	}
 	return 0;
 }
